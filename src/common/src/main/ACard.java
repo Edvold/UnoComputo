b55bdf68797443aaffc0f0ca6f30b3182ee4eae3
Marcus <69986002+marc6030@users.noreply.github.com>
2023-01-09 14:42:16 +0100

    Update ACard.java

--- src/common/src/main/ACard.java
+++ src/common/src/main/ACard.java
@@ -4,13 +4,29 @@ package common.src.main;
 public abstract class ACard implements Comparable<ACard> {
 	public abstract Color getColor();
 	public abstract Action getAction();
-	@Override
-	public abstract boolean equals(Object other);
 	public abstract boolean canBePlayedOn(ACard card);
 	public abstract void setColor(Color color);
 	public abstract boolean canChainWith(ACard other);
 	public abstract void resetWildCard();
 	@Override
-	public abstract int compareTo(ACard other);
+	public boolean equals(Object other){
+		if( !(other instanceof ACard) ){
+			return false;
+		}
+		ACard otherCard = (ACard) other;
+		if (!(this.getColor() == otherCard.getColor())){
+			return false;
+		}
+		if (!(this.getAction() == otherCard.getAction())){
+			return false;
+		}		
+		return true;
+	}
+	@Override
+	public int compareTo(ACard other){
+		int colorCompare = this.getColor().compareTo(other.getColor());
+		if (colorCompare != 0) return colorCompare;
+		return this.getAction().compareTo(other.getAction());
+	}
 }
 
