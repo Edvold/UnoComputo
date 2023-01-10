610db7764429a6f13cd2f830d045950372e7b47d
Marcus <69986002+marc6030@users.noreply.github.com>
2023-01-10 12:11:02 +0100

    Add Draw and edit in shuffledeck

--- src/common/src/main/Game.java
+++ src/common/src/main/Game.java
@@ -1,6 +1,7 @@
 package common.src.main;
 
 import org.jspace.ActualField;
+import org.jspace.FormalField;
 import org.jspace.RandomSpace;
 import org.jspace.Space;
 
@@ -14,7 +15,7 @@ import static common.src.main.Action.*;
 
 public class Game implements IGame{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Game g = new Game();
         g.generateDeck();
     }
@@ -47,18 +48,27 @@ public class Game implements IGame{
     @Override
     public void shuffleDeck() {
         ACard topCard = discardPile.pop();
-        generateDeck();
-        try {
-            deck.get(new ActualField(new Card(topCard.getColor(), topCard.getAction())));
-        } catch (InterruptedException e) {
+        try
+        {
+            for(ACard c : discardPile){
+                deck.put(new Card(c.getColor(), c.getAction()));
+            }
+        } catch (Exception e){
             e.printStackTrace();
-        }        
+        }
+        discardPile.push(topCard);     
     }
 
     @Override
-    public ACard[] draw(int amount) {
-        // TODO Auto-generated method stub
-        return null;
+    public ACard[] draw(int amount) throws InterruptedException {
+        if(amount > deck.size()){
+            shuffleDeck();
+        }
+        ACard[] cards = new ACard[amount];
+        for(int i = 0; i < amount;i++){
+            cards[i] = (ACard) (deck.get(new FormalField(ACard.class))[0]);
+        }
+        return cards;
     }
 
     @Override
--- src/common/src/main/IGame.java
+++ src/common/src/main/IGame.java
@@ -15,7 +15,7 @@ interface IGame {
      * Takes all but the top card of the discardPile and shuffles them into the deck
      */
     void shuffleDeck();
-    ACard[] draw(int amount);
+    ACard[] draw(int amount) throws InterruptedException;
     void startNextRound(IPlayer[] players); // Needs to take into account skipping & reversing
     boolean isObjectionCorrect();
 
