3eabcd2055ea5f13fa270b2b6cca21b93216b338
BjarkeBak <bjarkejensen@live.dk>
2023-01-10 10:12:15 +0100

    Changed IPlayer verify method

--- src/common/src/main/IPlayer.java
+++ src/common/src/main/IPlayer.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public interface IPlayer {
 
-    boolean verify(ACard card, ACard topCard); //verify that the card is playable
+    void addToOutput(ACard card); //Add card to list of cards to be played and remove the card from hand 
 
     ArrayList<ACard> getPlayableCards(ACard[] hand, ACard topCard); //find all playable cards from the player's hand
 
