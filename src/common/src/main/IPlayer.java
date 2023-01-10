27100ac64df50772524b71fa82e260acc1ad8e9f
BjarkeBak <bjarkejensen@live.dk>
2023-01-10 09:25:17 +0100

    added comments to IPlayer

--- src/common/src/main/IPlayer.java
+++ src/common/src/main/IPlayer.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 
 public interface IPlayer {
 
-    boolean verify(ACard card, ACard topCard);
+    boolean verify(ACard card, ACard topCard); //verify that the card is playable
 
-    ArrayList<ACard> getPlayableCards(ACard[] hand, ACard topCard);
+    ArrayList<ACard> getPlayableCards(ACard[] hand, ACard topCard); //find all playable cards from the player's hand
 
     String computeReturnToken(String ID); //can maybe be omitted
     
