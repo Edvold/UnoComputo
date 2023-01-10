bcc9789597406f822880f4a48d837d2a8a28dddc
BjarkeBak <bjarkejensen@live.dk>
2023-01-10 09:15:12 +0100

    Made constructor for GameState

--- src/common/src/main/GameState.java
+++ src/common/src/main/GameState.java
@@ -2,9 +2,17 @@ package common.src.main;
 
 public class GameState {
     
-    private String currentPlayerName;
-    private ACard topCard;
-    private IPlayer[] players;
-    private byte streak;
-    private ACard[] hand; //maybe this should just be directly through game and player
+    final public String currentPlayerName;
+    final public ACard topCard;
+    final public IPlayer[] players;
+    final public byte streak;
+    final public ACard[] hand; //maybe this should just be directly through game and player
+
+    public GameState (String currentPlayerName, ACard topCard, IPlayer[] players, byte streak, ACard[] hand){
+        this.currentPlayerName = currentPlayerName;
+        this.topCard = topCard;
+        this.players = players;
+        this.streak = streak;
+        this.hand = hand;
+    }
 }
