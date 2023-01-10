80ab44d03ec749c7cf88e8a8554395f46c7419c1
Edvold <56292479+Edvold@users.noreply.github.com>
2023-01-10 13:06:14 +0100

    Removed hand from gamestate

--- src/common/src/main/GameState.java
+++ src/common/src/main/GameState.java
@@ -6,13 +6,11 @@ public class GameState {
     final public ACard topCard;
     final public IPlayer[] players;
     final public byte streak;
-    final public ACard[] hand; //maybe this should just be directly through game and player
 
-    public GameState (String currentPlayerName, ACard topCard, IPlayer[] players, byte streak, ACard[] hand){
+    public GameState (String currentPlayerName, ACard topCard, IPlayer[] players, byte streak){
         this.currentPlayerName = currentPlayerName;
         this.topCard = topCard;
         this.players = players;
         this.streak = streak;
-        this.hand = hand;
     }
 }
