b815f46f417534a7f41443340649ef5beec8bcb9
Edvold <56292479+Edvold@users.noreply.github.com>
2023-01-11 11:40:11 +0100

    Example of connection between host and user

--- src/common/src/main/App.java
+++ src/common/src/main/App.java
@@ -1,17 +1,45 @@
 package common.src.main;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.rmi.Remote;
+import java.util.Scanner;
+
 import org.jspace.FormalField;
 import org.jspace.RandomSpace;
+import org.jspace.RemoteSpace;
+import org.jspace.SequentialSpace;
 import org.jspace.Space;
+import org.jspace.SpaceRepository;
 
 public class App {
 
-	public static void main(String[] argv) throws InterruptedException {
-		Space inbox = new RandomSpace();
+	public static void main(String[] argv) throws UnknownHostException, IOException, InterruptedException {
 
-		inbox.put("Hello World!");
-		Object[] tuple = inbox.get(new FormalField(String.class));
-		System.out.println(tuple[0]);
+
+			// (HOST) Set up repo
+			SpaceRepository repo = new SpaceRepository();
+			String ip = InetAddress.getLocalHost().getHostAddress();
+			
+			Space inbox = new SequentialSpace();
+			repo.add("inbox", inbox);
+			
+			repo.addGate("tcp://" + ip +":9001/?keep");
+
+			// (USER) Get ip addr of host @ user
+			Scanner scanner = new Scanner(System.in);
+			String hostIP = scanner.next();
+			scanner.close();
+			
+			// (USER) Connect to host's space
+			RemoteSpace space = new RemoteSpace("tcp://" + hostIP + ":9001/inboxB?keep");
+			space.put("Message from player :)");
+
+			// (HOST) get message
+			inbox.get(new FormalField(String.class));
+
+
 
 	}
 
