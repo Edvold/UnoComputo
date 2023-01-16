5bc40096df3741f320a467ceab34e22fc7fba05e
Edvold <56292479+Edvold@users.noreply.github.com>
2023-01-16 17:11:03 +0100

    Possible bugfix

--- src/common/src/main/Player.java
+++ src/common/src/main/Player.java
@@ -67,7 +67,7 @@ public class Player implements IPlayer {
                         actions.remove(PlayerAction.PLAY);
                     }
 
-                    UISpace.put(new PlayerMessage(gameState, playables.toArray(new Card[playables.size()]), hand.toArray(new Card[hand.size()]),
+                    UISpace.put(new PlayerMessage(gameState, playables.toArray(new Card[0]), hand.toArray(new Card[0]),
                             (PlayerAction[])actions.toArray(new PlayerAction[0])).getFields()); // message?
                     var newMessage = playerInbox.get(IMessage.getGeneralTemplate().getFields());
                     if (newMessage[0] == MessageType.CallOutCommand) {
