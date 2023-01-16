6977653c1987cbb4dbf19ae1c5c63d384abfe929
Goju Ryu <mads1606@gmail.com>
2023-01-16 13:09:53 +0100

    Fix start game flow and improve startNextTurn

--- src/common/src/main/Player.java
+++ src/common/src/main/Player.java
@@ -17,8 +17,8 @@ import common.src.main.Messages.UpdateMessage;
 public class Player implements IPlayer {
 
     private String playerName;
-    private ArrayList<ACard> hand = new ArrayList<>();
-    private ArrayList<ACard> output = new ArrayList<>();
+    private ArrayList<Card> hand = new ArrayList<>();
+    private ArrayList<Card> output = new ArrayList<>();
     private GameState gameState;
     private Space playerInbox;
     private Space UISpace;
@@ -28,13 +28,13 @@ public class Player implements IPlayer {
     private PlayerAction[] actions;
     private boolean playedFirstCard;
 
-    public Player (String name, Space gameSpace, Space UISpace, Space playerInbox) {
+    public Player(String name, Space gameSpace, Space UISpace, Space playerInbox) {
         playerName = name;
         this.UISpace = UISpace;
         this.gameSpace = gameSpace;
         this.playerInbox = playerInbox;
         callOutCheckerThread = new Thread(new CallOutChecker(playerInbox, gameSpace, playerName));
-        }
+    }
 
     @Override
     public void run() throws InterruptedException {
@@ -42,79 +42,89 @@ public class Player implements IPlayer {
         String token = "";
         while (true) {
             playedFirstCard = false;
-            var turnMessage = gameSpace.get(new ActualField(MessageType.NextPlayerCommand.getClass()), new FormalField(String.class), new FormalField(String.class));
-            this.gameState = ((GameState) gameSpace.get(new ActualField(MessageType.NewGameState.getClass()), new FormalField(GameState.class), new FormalField(String.class))[1]);
+            var turnMessage = playerInbox.get(new ActualField(MessageType.NextPlayerCommand),
+                    new FormalField(String.class), new FormalField(String.class));
+            this.gameState = ((GameState) playerInbox.get(new ActualField(MessageType.NewGameState),
+                    new FormalField(GameState.class), new FormalField(String.class))[1]);
             token = (String) turnMessage[1];
             if (token.equals("turnToken")) {
-            // It is your turn
+                // It is your turn
                 computeInitialActions(token);
                 while (token.equals("turnToken")) {
-                    ArrayList<ACard> playables = playedFirstCard || gameState.streak > 0 ? getStackingCards(hand, gameState.topCard) : getPlayableCards(hand, gameState.topCard);
-                    UISpace.put(new PlayerMessage(gameState, (Card[]) playables.toArray(), (Card[]) hand.toArray(), actions)); //message?
+                    ArrayList<Card> playables = playedFirstCard || gameState.streak > 0
+                            ? getStackingCards(hand, gameState.topCard)
+                            : getPlayableCards(hand, gameState.topCard);
+                    UISpace.put(new PlayerMessage(gameState, playables.toArray(new Card[playables.size()]), hand.toArray(new Card[hand.size()]),
+                            actions).getFields()); // message?
                     var newMessage = playerInbox.get(IMessage.getGeneralTemplate().getFields());
-                    if(newMessage[0] == MessageType.CallOutCommand){
-                        UISpace.put(new UpdateMessage("There has been an objection by " + newMessage[1]));
-                    }
-                    else if (newMessage[0] == MessageType.Update){
-                        UISpace.put(new UpdateMessage((String) newMessage[1]));
-                    }
-                    else if (newMessage[0] == MessageType.UIMessage){
+                    if (newMessage[0] == MessageType.CallOutCommand) {
+                        UISpace.put(new UpdateMessage("There has been an objection by " + newMessage[1]).getFields());
+                    } else if (newMessage[0] == MessageType.Update) {
+                        UISpace.put(new UpdateMessage((String) newMessage[1]).getFields());
+                    } else if (newMessage[0] == MessageType.UIMessage) {
                         PlayerAction action = (PlayerAction) newMessage[1];
                         switch (action) {
-                            case DRAW: String reason = gameState.streak > 0 ? "Streak" : "Draw";
-                                gameSpace.put(new DrawCardsCommand(reason));
+                            case DRAW:
+                                String reason = gameState.streak > 0 ? "Streak" : "Draw";
+                                gameSpace.put(new DrawCardsCommand(reason).getFields());
                                 getDrawnCards();
-                                    token = "";
-                                    break;
-                            case ENDTURN:  gameSpace.put(new PlayCardsCommand(saidUNO, output));
-                                    token = "";
-                                    break;
-                            case UNO:  saidUNO = true;
-                                    break;
-                            case OBJECT: gameSpace.put(new CallOutCommand(playerName));
-                                    break;
-                            case PLAY: ACard playedCard = hand.get(Integer.parseInt((String) newMessage[2]));
-                                    gameState.topCard = (Card) playedCard;
-                                    playedFirstCard = true;
-                                    if (playedCard.getAction().equals(Action.DRAW2) || playedCard.getAction().equals(Action.WILDDRAW4)){
-                                        gameState.streak++;
-                                    }
-                                    addToOutput(playedCard);
-                                    hand.remove(playedCard);
-                                    actions = new PlayerAction[] {PlayerAction.PLAY,PlayerAction.UNO,PlayerAction.OBJECT,PlayerAction.ENDTURN};
-                                    break;
+                                token = "";
+                                break;
+                            case ENDTURN:
+                                gameSpace.put(new PlayCardsCommand(saidUNO, output).getFields());
+                                token = "";
+                                break;
+                            case UNO:
+                                saidUNO = true;
+                                break;
+                            case OBJECT:
+                                gameSpace.put(new CallOutCommand(playerName).getFields());
+                                break;
+                            case PLAY:
+                                Card playedCard = hand.get(Integer.parseInt((String) newMessage[2]));
+                                gameState.topCard = (Card) playedCard;
+                                playedFirstCard = true;
+                                if (playedCard.getAction().equals(Action.DRAW2)
+                                        || playedCard.getAction().equals(Action.WILDDRAW4)) {
+                                    gameState.streak++;
+                                }
+                                addToOutput(playedCard);
+                                hand.remove(playedCard);
+                                actions = new PlayerAction[] { PlayerAction.PLAY, PlayerAction.UNO, PlayerAction.OBJECT,
+                                        PlayerAction.ENDTURN };
+                                break;
+                        }
                     }
                 }
-            }
-        }
-            else {
+            } else {
                 computeInitialActions(token);
                 // It is not your turn
-                if(!callOutCheckerThread.isAlive()){
+                if (!callOutCheckerThread.isAlive()) {
                     callOutCheckerThread.start();
                 }
-                UISpace.put(new PlayerMessage(gameState, new Card[0], (Card[]) hand.toArray(), actions));
-        }
+                UISpace.put(new PlayerMessage(gameState, new Card[0], hand.toArray(new Card[hand.size()]), actions).getFields());
+            }
         }
     }
 
     @Override
-    public void addToOutput(ACard card) {
+    public void addToOutput(Card card) {
         output.add(card);
     }
 
     @Override
-    public ArrayList<ACard> getPlayableCards(ArrayList<ACard> hand, ACard topCard) {
-        //finds playable cards
-        ArrayList<ACard> playables = new ArrayList<>(hand); //This will work if cards aren't changed until after they are played
+    public ArrayList<Card> getPlayableCards(ArrayList<Card> hand, Card topCard) {
+        // finds playable cards
+        ArrayList<Card> playables = new ArrayList<>(hand); // This will work if cards aren't changed until after they
+                                                           // are played
         playables.removeIf(card -> !card.canBePlayedOn(topCard));
         return playables;
     }
 
-    public ArrayList<ACard> getStackingCards(ArrayList<ACard> hand, ACard topCard){
-        ArrayList<ACard> stackables = new ArrayList<>();
-        for (ACard card : hand){
-            if (card.getAction().equals(topCard.getAction())){
+    public ArrayList<Card> getStackingCards(ArrayList<Card> hand, Card topCard) {
+        ArrayList<Card> stackables = new ArrayList<>();
+        for (Card card : hand) {
+            if (card.getAction().equals(topCard.getAction())) {
                 stackables.add(card);
             }
         }
@@ -123,46 +133,45 @@ public class Player implements IPlayer {
 
     @Override
     public String computeReturnToken(String ID) {
-        return  ID.equals("object") ? "null" : "TurnToken";
-        //This method is not needed
+        return ID.equals("object") ? "null" : "TurnToken";
+        // This method is not needed
     }
 
-    //getters and setters
+    // getters and setters
 
-    public void setHand(ArrayList<ACard> newHand) {
+    public void setHand(ArrayList<Card> newHand) {
         hand = newHand;
-    }    
+    }
 
     private void getDrawnCards() throws InterruptedException {
         var template = new DrawCardsCommand().getTemplateBuilder()
-            .addActualType()
-            .build();
+                .addActualType()
+                .build();
         var response = playerInbox.get(template.getFields());
         var message = (DrawCardsCommand) MessageFactory.create(response);
 
         var newCards = Arrays.asList(message.getState());
 
         this.hand.addAll(newCards);
-    } 
+    }
 
     public void computeInitialActions(String token) {
-        if (token.equals("turnToken")){
-            actions = new PlayerAction[] {PlayerAction.PLAY,PlayerAction.DRAW,PlayerAction.OBJECT,PlayerAction.UNO};
-        }
-        else {
-            actions = new PlayerAction[] {PlayerAction.OBJECT};
+        if (token.equals("turnToken")) {
+            actions = new PlayerAction[] { PlayerAction.PLAY, PlayerAction.DRAW, PlayerAction.OBJECT,
+                    PlayerAction.UNO };
+        } else {
+            actions = new PlayerAction[] { PlayerAction.OBJECT };
         }
     }
-    
-}
 
+}
 
 class CallOutChecker implements Runnable {
     private Space checkingSpace;
     private Space sendingSpace;
     private String playerName;
 
-    public CallOutChecker(Space checkSpace, Space sendSpace, String name){
+    public CallOutChecker(Space checkSpace, Space sendSpace, String name) {
         this.checkingSpace = checkSpace;
         this.sendingSpace = sendSpace;
         this.playerName = name;
@@ -170,9 +179,10 @@ class CallOutChecker implements Runnable {
 
     public void run() {
         try {
-            while(true){
-                checkingSpace.get(new ActualField(MessageType.UIMessage.getClass()), new ActualField(PlayerAction.OBJECT.getClass()), new FormalField(String.class));
-                sendingSpace.put(new CallOutCommand(playerName));
+            while (true) {
+                checkingSpace.get(new ActualField(MessageType.UIMessage), new ActualField(PlayerAction.OBJECT),
+                        new FormalField(String.class)); 
+                sendingSpace.put(new CallOutCommand(playerName).getFields());
                 return;
             }
         } catch (InterruptedException e) {
