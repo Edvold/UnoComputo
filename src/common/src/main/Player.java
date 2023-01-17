c4a55ee3c7ae57da470b3c65180f2ad7d3d3e476
Edvold <56292479+Edvold@users.noreply.github.com>
2023-01-17 09:34:29 +0100

    bugfixing regarding objection

--- src/common/src/main/Player.java
+++ src/common/src/main/Player.java
@@ -106,7 +106,6 @@ public class Player implements IPlayer {
                                 break;
                             case OBJECT:
                                 gameSpace.put(new CallOutCommand(playerName).getFields());
-                                sendNewGameState = false;
                                 break;
                             case PLAY:
 
